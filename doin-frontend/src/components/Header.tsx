c91cc06ee075c27288624ab4f055222841c34c45
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-12 15:24:07 +0530

    add: added connection loader

--- doin-frontend/src/components/Header.tsx
+++ doin-frontend/src/components/Header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { useEthers, useTokenBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+import { toast } from "react-toastify";
 
 const Header: React.FC = () => {
+  const [accountData, setAccountData] = React.useState<any>(null); // storing account data
+
+  const { account, activateBrowserWallet, chainId } = useEthers();
+
+  // storing the balance shown in the header
+
+  React.useEffect(() => {
+    if (chainId === 4 || chainId === 97) {
+      setAccountData(account);
+    }
+  }, [account, chainId]);
+
+  console.log(accountData);
+
   return (
     <Navbar bg="dark" expand="xxl">
       <Container>
@@ -34,7 +51,20 @@ const Header: React.FC = () => {
         </Nav>
       </Container>
       <Container className="justify-content-end flex-grow-1 pe-3">
-        <Button variant="primary">Connect</Button>
+        <Button
+          variant="primary"
+          onClick={
+            !account
+              ? () => activateBrowserWallet()
+              : () => toast.info("You are already logged in")
+          }
+        >
+          {accountData
+            ? `${accountData?.substring(0, 4)}...${accountData?.substring(
+                accountData.length - 4
+              )}`
+            : "Connect"}
+        </Button>
       </Container>
     </Navbar>
   );
--- doin-frontend/src/index.tsx
+++ doin-frontend/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ChainId, Config, DAppProvider } from "@usedapp/core";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const config: Config = {
   supportedChains: [ChainId.Rinkeby, 97],
