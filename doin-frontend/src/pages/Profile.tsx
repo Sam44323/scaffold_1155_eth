9e3115f8c8ff165f2ed614d99846bb8a8bac1127
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-12 21:37:32 +0530

    add: added styler for profile

--- doin-frontend/src/pages/Profile.tsx
+++ doin-frontend/src/pages/Profile.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "../styles/pages/Profile.module.css";
+import { NftPortAxios } from "../helpers/axios";
+import { useEthers } from "@usedapp/core";
 
 const Profile: React.FC = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={styles.ProfileContainer}>
+      <section className={styles.HeaderContainer}>
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "3.5rem",
+            backgroundColor: "#000",
+            padding: "1rem",
+            color: "#fff",
+          }}
+        >
+          Profile
+        </h1>
+      </section>
+    </div>
+  );
 };
 
 export default Profile;
