e8c3299ebac7929f5c6a9176739150762540ce14
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-12 14:47:54 +0530

    add: added support for frontend

---
+++ solidity_contracts/contracts/DoinNFT.sol
@@ -0,0 +1,8 @@
+// contracts/NFT.sol
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/exte/ERC1155/ERC1155.sol";
+
+contract DoinNFT is ERC1155, {}
